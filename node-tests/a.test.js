c639fdc319326b4305f6e189457a23f3b2cffd55
Ben Lowery <14350+blowery@users.noreply.github.com>
2023-11-30 12:12:58 -0500

    clean up and isolate jest and node tests, add bun runner

--- node-tests/a.test.js
+++ node-tests/a.test.js
@@ -1,12 +1,14 @@
-const test = require('node:test');
-const {setTimeout} = require('node:timers/promises');
+const { test, describe } = require('node:test');
+const { setTimeout } = require('node:timers/promises');
 
-test('a', () => {
-    // not necessary, just making linters happy
-    console.log('a done...');
-    // wait 3 seconds and then fail.
-    // this happens after test cleanup..
-    setTimeout(1000).then(() => { 
-        throw new Error('test "a" threw!')
-    });
-})
+describe('node-tests', () => {
+    test('a', () => {
+        // not necessary, just making linters happy
+        console.log('a done...');
+        // wait 3 seconds and then fail.
+        // this happens after test cleanup..
+        setTimeout(1000).then(() => {
+            throw new Error('test "a" threw!')
+        });
+    })
+});
--- node-tests/b.test.js
+++ node-tests/b.test.js
@@ -1,8 +1,10 @@
-const test = require('node:test');
+const { test, describe } = require('node:test');
 const { setTimeout } = require('node:timers/promises');
 
-test('b', async () => {
-    // wait for a bit to let promises from test-a to settle
-    await setTimeout(2000);
-    console.log('b done');
-})
+describe('node-tests', () => {
+    test('b', async () => {
+        // wait for a bit to let promises from test-a to settle
+        await setTimeout(2000);
+        console.log('b done');
+    });
+});
